1083ab83f753cce8aba97625f270afc41696459b
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-02 01:24:23 +0900

    chore

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pick = pick;
 function pick(keys, obj) {
     const x = [...keys].reduce((pre, current) => (Object.assign(Object.assign({}, pre), { [current]: obj[current] })), {});
     return x;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export function pick(keys: string[], obj: unknown) {
+function pick(keys: string[], obj: unknown) {
 	const x = [...keys].reduce(
 		(pre, current) => ({ ...pre, [current]: obj[current] as any }),
 		{},
