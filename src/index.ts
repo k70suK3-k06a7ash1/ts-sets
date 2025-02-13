7d61eb36c5e0f548d7a13cdb44f9dce372d9cc0d
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-13 22:56:34 +0900

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export function pick(keys: string[], obj: any) {
 	const x = [...keys].reduce(
+		// biome-ignore lint/performance/noAccumulatingSpread: <explanation>
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 		(pre, current) => ({ ...pre, [current]: obj[current] as any }),
 		{},
 	);
